2e7650d8c630ad633f9a6976501d8af0ec96f32c
mansk <mansk@users.noreply.github.com>
2024-11-15 14:47:08 +0000

    Add docs to list

--- src/main.py
+++ src/main.py
@@ -3,10 +3,13 @@ import tarfile
 
 nlp = spacy.load("en_core_web_sm")
 
+docs = []
+
 with tarfile.open("data.tar.gz", mode="r:gz") as tar_data:
     for member in tar_data.getmembers():
         if member.isfile() and member.name.endswith(".txt"):
             file = tar_data.extractfile(member).read()
             doc = nlp(file.decode("utf_8"))
+            docs.append(doc)
 
             print(f"{sum(1 for _ in doc.sents)} sentences found in {member.name}")
